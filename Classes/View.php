7c344db3b13d41b9eeb1fb9d1ad4398962123bf0
webpajooh <webpajooh@gmail.com>
2020-12-11 14:26:50 +0330

    View class created!

--- Classes/View.php
+++ Classes/View.php
@@ -4,5 +4,12 @@ namespace PCX\Classes;
 
 class View
 {
-    
+    public static function make($viewName, $data = [])
+    {
+        $viewFile = dirname(__FILE__) . '/../Views/' . $viewName . '.php';
+
+        extract($data, EXTR_SKIP);
+
+        require $viewFile;
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
+use PCX\Classes\View;
+
 require './vendor/autoload.php';
+
+$title = 'Hello, world!';
+$author = 'WebPajooh';
+View::make('view1', compact('title', 'author'));
